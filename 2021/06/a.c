a66f37da00de7ad1cdf755041aa2ad01caabc0e4
Owen Jones <owen@owenjones.net>
2021-12-06 23:08:35 +0000

    complete! (answer=388739)

---
+++ 2021/06/a.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  int count;
+  int _size;
+  int* members;
+} population;
+
+void init(population* p) {
+  p->count = 0; // total count
+  p->members = calloc(1000000, sizeof(int));
+}
+
+void disp_population(population* p) {
+  for(int i = 0; i < p->count; i++) {
+    printf("%i,", p->members[i]);
+  }
+  printf("\n");
+}
+
+void add_to_population(population* p, int age) {
+  p->members[p->count] = age;
+  p->count++;
+  
+  // if(p->count > p->_size) {
+  //   exit(1);
+  // }
+}
+
+void tick(population* p) {
+  int n = p->count;
+  for(int i = 0; i < n; i++) {
+    p->members[i]--;
+    if(p->members[i] < 0) {
+      p->members[i] = 6;
+      add_to_population(p, 8);
+    }
+  }
+}
+
+int main(void) {
+  char file[] = "input.txt";
+  FILE* fptr;
+
+  if((fptr = fopen(file, "r")) == NULL) {
+    printf("Error opening file\n");
+    exit(1);
+  }
+
+  population* p = calloc(1, sizeof(population));
+  init(p);
+  
+  int n;
+  while(fscanf(fptr, "%i,", &n) > 0) {
+    add_to_population(p, n);
+  }
+
+  fclose(fptr);
+
+  for(int d = 1; d <= 80; d++) {
+    tick(p);
+    printf("population after %i days = %i\n", d, p->count);
+    // disp_population(p);
+  }
+
+  return 0;
+}
