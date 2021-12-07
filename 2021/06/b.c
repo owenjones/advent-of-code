b3974590cd1720ff5c6c48050aa2bc8555bef2b7
Owen Jones <owen@owenjones.net>
2021-12-07 10:10:51 +0000

    tried a different approach - complete! (answer=1741362314973)

--- 2021/06/b.c
+++ 2021/06/b.c
@@ -3,30 +3,6 @@
 #include <string.h>
 #include <inttypes.h>
 
-typedef struct fish {
-  int8_t value;
-  struct fish* next;
-} fish_t;
-
-int count(fish_t* head) {
-  fish_t* fish = head;
-  int count = 0;
-  while(fish != NULL) {
-    count++;
-    fish = fish->next;
-  }
-  
-  return count;
-}
-
-void disp_pop(fish_t* head) {
-  while(head != NULL) {
-    printf("%i,", head->value);
-    head = head->next;
-  }
-  printf("\n");
-}
-
 int main(void) {
   char file[] = "input.txt";
   FILE* fptr;
@@ -36,51 +12,30 @@ int main(void) {
     exit(1);
   }
 
-  fish_t* head = NULL;
-  fish_t* fish;
-  int n;
-  while(fscanf(fptr, "%i,", &n) > 0) {
-    if(!head) {
-      fish = (fish_t*) malloc(sizeof(fish_t));
-      head = fish;
-    } else {
-      fish->next = (fish_t*) malloc(sizeof(fish_t));
-      fish = fish->next;
-    }
-    fish->value = n;
-  }
+  uint64_t fish[9];
+  for(size_t i = 0; i < 9; i++) fish[i] = 0;
+
+  uint8_t n;
+  while(fscanf(fptr, "%i,", &n) > 0) fish[n]++;
 
   fclose(fptr);
 
-  printf("Initial size = %i\n", count(head));
-  // disp_pop(head);
+  uint64_t c = 0;
+  for(size_t i = 0; i < 9; i++) c += fish[i];
+  printf("population after 0 days = %i\n", c);
 
-  fish_t* last;
-  for(int d = 0; d < 256; d++) {
-    printf("Day %i\n", d);
-    fish = head;
-    int add = 0;
-    while(fish != NULL) {
-      fish->value--;
-      
-      if(fish->value < 0) {
-        fish->value = 6;
-        add++;
-      }
-      
-      last = fish;
-      fish = fish->next;
-    }
-    
-    for(int i = 0; i < add; i++) {
-      last->next = (fish_t*) malloc(sizeof(fish_t));
-      last = last->next;
-      last->value = 8;
-    }
+  uint64_t recycle;
+  size_t d;
+  for(d = 0; d < 256; d++) {
+    recycle = fish[0];
+    for(size_t i = 0; i < 8; i++) fish[i] = fish[i+1];
+    fish[6] += recycle;
+    fish[8] = recycle;
   }
-  
-  int c = count(head);
-  printf("population after 256 days = %i\n", c);
+
+  c = 0;
+  for(size_t i = 0; i < 9; i++) c += fish[i];
+  printf("population after %i days = %llu\n", d, c);
 
   return 0;
 }
