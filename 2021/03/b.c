117b8cc880fcd755bb882f333f09d6fe98c7674f
Owen Jones <owen@owenjones.net>
2021-12-08 13:43:51 +0000

    complete! (problem was I wasn't re-calculating most/least common bit after filtering)

--- 2021/03/b.c
+++ 2021/03/b.c
@@ -6,10 +6,8 @@
 int btoi(int input[12]) {
   int out = 0;
   for(size_t i = 0, j = 11; i < 12; i++, j--) {
-    printf("%i", input[i]);
     out += input[i] * pow(2, j);
   }
-  printf("\n");
   return out;
 }
 
@@ -29,12 +27,56 @@ int main(void) {
       fscanf(fptr, "%1i", &inputs[i][j]);
     }
   }
-
   fclose(fptr);
 
-  
+  int o2s[1000][12], co2s[1000][12];
+  memcpy(&o2s, &inputs, sizeof(int)*1000*12);
+  memcpy(&co2s, &inputs, sizeof(int)*1000*12);
 
-  int life = o2 * co2;
-  printf("\nO2 Generator: %i\nCO2 Scrubber: %i\nLife Support Rating: %i\n", o2, co2, life);
+  int o2 = 0, co2 = 0;
+  int o2i, co2i;
+  int o2n = 1000, co2n = 1000;
+  for(size_t b = 0; b < 12; b++) {
+    o2i = 0, co2i = 0;
+
+    // find most common bit in O2 list
+    int n = 0, m = 0;
+    for(size_t i = 0; i < o2n; i++) n += o2s[i][b];
+    m = (2*n >= o2n);
+
+    // filter O2 list to keep inputs with matching bit
+    if(o2 == 0) {
+      for(size_t i = 0; i < o2n; i++) {
+        if(o2s[i][b] == m) {
+          memcpy(&o2s[o2i], &o2s[i], sizeof(int)*12);
+          o2i++;
+        }
+      }
+    }
+
+    // find most common bit in CO2 list
+    n = 0, m = 0;
+    for(size_t i = 0; i < co2n; i++) n += co2s[i][b];
+    m = (2*n < co2n);
+
+    // filter CO2 list to keep inputs without matching bit
+    if(co2 == 0) {
+      for(size_t i = 0; i < co2n; i++) {
+        if(co2s[i][b] == m) {
+          memcpy(&co2s[co2i], &co2s[i], sizeof(int)*12);
+          co2i++;
+        }
+      }
+    }
+
+    if(o2i == 1 && o2 == 0) o2 = btoi(o2s[0]);
+    if(co2i == 1 && co2 == 0) co2 = btoi(co2s[0]);
+
+    memcpy(&o2n, &o2i, sizeof(int));
+    memcpy(&co2n, &co2i, sizeof(int));
+  }
+
+  int life = o2 * co2; // 3368358
+  printf("O2 Generator: %i\nCO2 Scrubber: %i\nLife Support Rating: %i\n", o2, co2, life);
   return 0;
 }
