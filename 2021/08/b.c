348d4730e0462d5466f01c62584185e4b6517c2d
Owen Jones <owen@owenjones.net>
2021-12-09 17:07:09 +0000

    mostly worked the logic out now

--- 2021/08/b.c
+++ 2021/08/b.c
@@ -15,32 +15,74 @@ line* parse_line(char inputs[15][8]) {
   return line;
 }
 
+int letter_to_index(char letter) {
+  // convert segment letters into an indexable value (a=0, b=1...)
+  return letter - 97;
+}
+
+char index_to_letter(int index) {
+  // convert indexes back into segment letters
+  return index + 97;
+}
+
+int get_value(int segmentv) {
+  // convert binary value of lit segments into display value
+  switch(segmentv) {
+    case 6: return 1;
+    case 7: return 7;
+    case 63: return 0;
+    case 79: return 3;
+    case 91: return 2;
+    case 102: return 4;
+    case 109: return 5;
+    case 111: return 9;
+    case 125: return 6;
+    case 127: return 8;
+  }
+}
+
+void sort(char inputs[10][8]) {
+
+}
+
 int solve_line(line* line) {
-  int total = 0;
-  
+  // calculate how often we see each segment letter
+  int frequencies[10];
+  for(size_t i = 0; i < 10; i++) frequencies[i] = 0;
   for(size_t i = 0; i < 10; i++) {
-    int len = strlen(line->inputs[i]);
-    switch(len) {
-      case 2:
-      case 3:
-      case 4:
-      case 7:
-        line->matched[len] = line->inputs[i];
-        break;
-      case 5:
-        break;
-        
-      case 6:
-        break;
+    for(size_t j = 0; j < strlen(line->inputs[i]); j++) {
+      frequencies[letter_to_index(line->inputs[i][j])]++;
     }
   }
 
-  for(size_t i = 0; i < 4; i++) {
-    for(size_t j = 0; j < 10; j++) {
-      if(strcmp(line->outputs[i], line->matched[j]) == 0) total += j);
+  // map frequencies to segments {S0,S2,S4,S5} (the easy ones...)
+  int values[10];
+  for(size_t i = 0; i < 10; i++) {
+    switch(frequencies[i]) {
+      case 4: values[i] = 16;
+      case 6: values[i] = 32;
+      case 8: values[i] = 1;
+      case 9: values[i] = 2;
+      default: values[i] = 0;
     }
   }
-  
+
+  sort(line->inputs);
+
+  // find S1 from 1 (input length 2)
+
+  // find S6 from 4 (input length 4)
+
+  // find S3 as the only remaining letter
+
+  // work through each lit segment in each of the output values, and add
+  // that segment's value to the total
+  int total = 0;
+  for(size_t i = 0; i < 4; i++) {
+    for(size_t j = 0; j < strlen(line->outputs[i]); j++) {
+      total += values[letter_to_index(line->outputs[i][j])];
+    }
+  }
   return total;
 }
 
