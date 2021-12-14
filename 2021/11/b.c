48aea16743d3766d6daf063dadde2a2dbf5491cf
Owen Jones <owen@owenjones.net>
2021-12-14 14:20:36 +0000

    very easy second part!

---
+++ 2021/11/b.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+void dispgrid(uint8_t* g) {
+  for(size_t i = 0; i < 100; i++) {
+    printf("%i", g[i]);
+    if((i + 1) % 10 == 0) printf("\n");
+  }
+  printf("\n");
+}
+
+int main(void) {
+  char file[] = "input.txt";
+  FILE* fptr;
+
+  if((fptr = fopen(file, "r")) == NULL) {
+    printf("Error opening file\n");
+    exit(1);
+  }
+
+  uint8_t* grid = (uint8_t*) calloc(100, sizeof(uint8_t));
+  size_t i = 0;
+  while(fscanf(fptr, "%1i", &grid[i]) > 0) i++;
+  fclose(fptr);
+
+  // for 100 steps:
+  // * step through all positions and increase by 1
+  // * step through all positions and check if there's a flash - mark as flashed,
+  //   increase all surrounding positions by 1 and restart from beginning of grid
+  // * once we've made it to end of grid, reset all flashed to 0 and move to next step
+
+  uint32_t flashes = 0;
+  uint8_t* flashed = (uint8_t*) calloc(100, sizeof(uint8_t));
+  size_t s;
+
+  for(s = 0; s < 1000; s++) {
+    for(size_t i = 0; i < 100; i++) grid[i]++;
+
+    size_t i = 0;
+    while(i < 100) {
+      if(grid[i] > 9 && flashed[i] == 0) {
+        // FLASH (ah ahhh)
+        flashes++;
+        flashed[i] = 1;
+
+        int x = i % 10;
+        int y = i / 10;
+
+        // work around adjacent cells clockwise from 12 o'clock.
+        if(y - 1 >= 0) grid[i - 10]++;
+        if(y - 1 >= 0 && x + 1 < 10) grid[i - 10 + 1]++;
+        if(x + 1 < 10) grid[i + 1]++;
+        if(y + 1 < 10 && x + 1 < 10) grid[i + 10 + 1]++;
+        if(y + 1 < 10) grid[i + 10]++;
+        if(y + 1 < 10 && x - 1 >= 0) grid[i + 10 - 1]++;
+        if(x - 1 >= 0) grid[i - 1]++;
+        if(y - 1 >= 0 && x - 1 >= 0) grid[i - 10 - 1]++;
+
+        // go back to start of the grid and check for new flashes
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+
+    int sum = 0;
+    for(size_t i = 0; i < 100; i++) {
+      sum += (int) flashed[i];
+      grid[i] = flashed[i] ? 0 : grid[i];
+      flashed[i] = 0;
+    }
+
+    if(sum == 100) {
+      printf("Flashed on step: %zu\n", (s+1));
+      return 0;
+    }
+  }
+
+  printf("No flash sync...\n");
+  return 1;
+}
