698f8907b31588606299e7859aa6ccb75a8b606e
Owen Jones <owen@owenjones.net>
2022-12-05 06:29:17 +0000

    day 5 - needs some memory management...

---
+++ 2022/05/b.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "input.txt"
+#define STACKS 9
+
+typedef struct crate {
+  char id;
+  struct crate* next;
+} crate_t;
+
+int main(void) {
+  char file[] = INPUT;
+  FILE* fptr;
+
+  if((fptr = fopen(file, "r")) == NULL) {
+    printf("Error opening file\n");
+    exit(1);
+  }
+
+  crate_t* stacks[STACKS];
+  for(size_t i = 0; i < STACKS; i++) {
+    stacks[i] = NULL;
+  }
+  
+  char* line = NULL;
+  size_t len;
+  int loop = 1;
+  
+  // fill stack structure with crates
+  while((getline(&line, &len, fptr) != -1) && loop) {
+    if(strcmp(line, " 1   2   3   4   5   6   7   8   9 \n") == 0) loop = 0;
+    //if(strcmp(line, " 1   2   3 \n") == 0) loop = 0;
+    
+    for(size_t i = 0; i < STACKS; i++) {
+      char c = line[(i*4) + 1];
+      if((int) c > 64 && (int) c < 91) {
+        crate_t* n = calloc(1, sizeof(crate_t));
+        n->id = c;
+        n->next = NULL;
+        
+        if(stacks[i] == NULL) {
+          stacks[i] = n;
+        } else {
+          crate_t* ptr = stacks[i];
+          crate_t* parent = NULL;
+          while(ptr != NULL) {
+            parent = ptr;
+            ptr = parent->next;
+          }
+          parent->next = n;
+        }
+      }
+    }
+  }
+  
+  int n, a, b;
+  while(fscanf(fptr, "move %i from %i to %i\n", &n, &a, &b) > 0) {
+    // DEBUG: show crate structure;
+    for(size_t i = 0; i < STACKS; i++) {
+      printf("%lu => ", i);
+      crate_t* ptr = stacks[i];
+      while(ptr != NULL) {
+        printf("%c ", ptr->id);
+        ptr = ptr->next;
+      }
+      printf("\n");
+    }
+    printf("\n");
+    
+    a -= 1; // correct indexes
+    b -= 1;
+    
+    crate_t* head = stacks[a];
+    crate_t* nhead = head;
+    crate_t* bottom = NULL;
+    for(size_t i = 0; i < n; i++) {
+      bottom = nhead;
+      nhead = nhead->next;
+    }
+    
+    stacks[a] = nhead;
+    bottom->next = stacks[b];
+    stacks[b] = head;
+  }
+
+  fclose(fptr);
+
+  printf("Top of stacks: ");
+  for(size_t i = 0; i < STACKS; i++) {
+    printf("%c", stacks[i]->id);
+  }
+  printf("\n");
+  
+  return 0;
+}
