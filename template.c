db94f0691dc583d973937ccc007769fcba177380
Owen Jones <owen@owenjones.net>
2022-12-05 23:59:32 +0000

    updated to how am starting now

--- template.c
+++ template.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 int main(void) {
-  char file[] = "input.txt";
-  int l = 1000;
   FILE* fptr;
-
-  if((fptr = fopen(file, "r")) == NULL) {
+  if((fptr = fopen("test_input.txt", "r")) == NULL) {
     printf("Error opening file\n");
     exit(1);
-  }
-
-  for(size_t i = 0; i < l; i++) {
-
   }
 
   fclose(fptr);
