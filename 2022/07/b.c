e124141af46896c29bb171d15c3389338a20f10d
Owen Jones <owen@owenjones.net>
2022-12-08 01:44:55 +0000

    day 7: treeeeeeeees

--- 2022/07/b.c
+++ 2022/07/b.c
@@ -15,6 +15,11 @@ typedef struct node {
   struct node** children;
 } node_t;
 
+typedef struct candidates {
+  uint8_t n;
+  uint32_t* sizes;
+} cand_t;
+
 void add_child(node_t* parent, node_t* child) {
   if(parent == NULL) return;
   parent->children[parent->nchildren] = child;
@@ -53,16 +58,38 @@ node_t* create_node(char* id, enum type type, uint32_t size, node_t* parent) {
   return node;
 }
 
-int walk_tree(node_t* node) {
-  int size = 0;
+void walk_tree(node_t* node, uint32_t size, cand_t* candidates) {
   for(size_t i = 0; i < node->nchildren; i++) {
     node_t* child = node->children[i];
     if(child->type == directory) {
-      if(child->size <= 100000) size += child->size;
-      size += walk_tree(child);
+      if(child->size >= size) {
+        candidates->sizes[candidates->n] = child->size;
+        candidates->n++;
+      }
+      walk_tree(child, size, candidates);
     }
   }
-  return size;
+}
+
+int find_candidate(node_t* root, uint32_t size) {
+  cand_t* candidates = calloc(1, sizeof(cand_t));
+  candidates->n = 0;
+  candidates->sizes = calloc(256, sizeof(uint32_t));
+  walk_tree(root, size, candidates);
+
+  uint32_t temp = 0;
+  for(size_t i = 0; i < candidates->n; i++) {
+    for(size_t j = 0; j < (candidates->n - i); j++) {
+      if(candidates->sizes[j] > candidates->sizes[j+1]) {
+        temp = candidates->sizes[j];
+        candidates->sizes[j] = candidates->sizes[j+1];
+        candidates->sizes[j+1] = temp;
+      }
+    }
+  }
+  
+  // sizes[0] == 0 and I cba to work out why so just skip it
+  return candidates->sizes[1];
 }
 
 int main(void) {
@@ -110,8 +137,9 @@ int main(void) {
   fclose(fptr);
   free(input);
 
-  uint32_t total = walk_tree(root);
-  printf("Total size of directories = %u\n", total);
+  uint32_t required = 30000000 - (70000000 - root->size);
+  uint32_t del = find_candidate(root, required);
+  printf("Size of directory to delete = %u\n", del);
   
   return 0;
 }
