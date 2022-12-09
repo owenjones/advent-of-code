6a4913f083a16a1b6d2647f5dc3800b8e72d97d2
Owen Jones <owen@owenjones.net>
2022-12-09 16:21:26 +0000

    was definitely making this harder than it needed to be...

--- 2022/09/a.c
+++ 2022/09/a.c
@@ -39,24 +39,10 @@ void set_direction(char direction, int* x, int* y) {
   }
 }
 
-int is_adjacent(coord_t* a, coord_t* b) {
-  int x = (abs(b->x - a->x) == 1);
-  int y = (abs(b->y - a->y) == 1);
+int is_neighbour(coord_t* a, coord_t* b) {
+  int x = (abs(b->x - a->x) <= 1);
+  int y = (abs(b->y - a->y) <= 1);
   return x && y;
-}
-
-int is_on_top(coord_t* a, coord_t* b) {
-  return (a->x == b->x) && (a->y == b->y);
-}
-
-int on_same_plane(char direction, char previous) {
-  if((direction == 'L' && previous == 'R') || 
-  (direction == 'R' && previous == 'L')) return 1;
-  
-  if((direction == 'U' && previous == 'D') || 
-  (direction == 'D' && previous == 'U')) return 1;
-  
-  return 0;
 }
 
 int manhattan_distance(coord_t* a, coord_t* b) {
@@ -65,42 +51,14 @@ int manhattan_distance(coord_t* a, coord_t* b) {
   return x + y;
 }
 
-int is_diagonal(coord_t* a, coord_t* b) {
-  return is_adjacent(a, b) && (manhattan_distance(a, b) == 2);
-}
-
 void mark_visited(int* list, int x, int y) {
-  printf("Visited (%d, %d)\n", x, y);
   list[c2ind(x, y)] = 1;
 }
 
-void move_head(coord_t* head, char direction, int steps) {
-  int x, y;
-  set_direction(direction, &x, &y);
-  head->x += (x * steps);
-  head->y += (y * steps);
-}
-
-void move_tail_linearly(coord_t* point, char direction, int steps, int* squares) {
-  if(steps < 0) return; // don't think this should happen, but while testing...
-  
-  int x, y;
-  set_direction(direction, &x, &y);
-  for(size_t i = 0; i < steps; i++) {
-    point->x += x;
-    point->y += y;
-    mark_visited(squares, point->x, point->y);
-  }
-}
-
-void move_tail_diagonally(coord_t* head, coord_t* tail, int* squares) {
+void set_diagonal_direction(coord_t* head, coord_t* tail, int* x, int* y) {
   // vector between head & tail - move 1 in that x&y
-  int dx = copysign(1, (head->x - tail->x));
-  int dy = copysign(1, (head->y - tail->y));
-  
-  tail->x += dx;
-  tail->y += dy;
-  mark_visited(squares, tail->x, tail->y);
+  *x = copysign(1, (head->x - tail->x));
+  *y = copysign(1, (head->y - tail->y));
 }
 
 int count_visited(int* list) {
@@ -111,7 +69,7 @@ int count_visited(int* list) {
 
 int main(void) {
   FILE* fptr;
-  if((fptr = fopen("test_input.txt", "r")) == NULL) {
+  if((fptr = fopen("input.txt", "r")) == NULL) {
     printf("Error opening file\n");
     exit(1);
   }
@@ -121,52 +79,48 @@ int main(void) {
 
   // head of rope, starts at the origin
   coord_t* head = calloc(1, sizeof(coord_t));
-  head->x = 0;
-  head->y = 0;
+  head->x = 500;
+  head->y = 500;
   
   // tail of rope, also starts at the origin
   coord_t* tail = calloc(1, sizeof(coord_t));
-  tail->x = -1;
-  tail->y = 0;
+  tail->x = 500;
+  tail->y = 500;
+  mark_visited(squares, 500, 500);
 
   char direction;
   char previous = 'R';
-  int steps, distance;
-  while(fscanf(fptr, "%c %d\n", &direction, &steps) > 0) {
-    printf("Read line (%c %d) - ", direction, steps);
-
-    move_head(head, direction, steps);
-    
-    // if head is now adjacent to tail (or on top of it) then we don't need to move it
-    if(is_adjacent(head, tail) || is_on_top(head, tail)) {
-      printf("head is adjacent/on top\n");
-      continue;
-    }
+  int steps, distance, x, y;
+  size_t i;
   
-    if(direction == previous) {
-      printf("moving in same direction\n");
-      move_tail_linearly(tail, direction, steps, squares);
-    } else if(on_same_plane(direction, previous)) {
-      printf("moving in same plane/opposite direction\n");
-      distance = manhattan_distance(head, tail);
-      move_tail_linearly(tail, direction, (steps - 3), squares);
-    } else {
-      printf("moving in opposite plane\n");
-      move_tail_diagonally(head, tail, squares);
-      move_tail_linearly(tail, direction, (steps - 2), squares);
-    }
+  while(fscanf(fptr, "%c %d\n", &direction, &steps) > 0) {
 
-    previous = direction;
+    for(i = 0; i < steps; i++) {
+      // move head 1 step in direction
+      set_direction(direction, &x, &y);
+      head->x += x;
+      head->y += y;
+      
+      // don't need to move tail unless it's not adjacant/underneath the head
+      if(is_neighbour(head, tail)) {
+        continue;
+      }
+      
+      if(manhattan_distance(head, tail) > 2) {
+        // head is diagonal from tail, need to move tail a step diagonally
+        set_diagonal_direction(head, tail, &x, &y);
+      }
+        
+      tail->x += x;
+      tail->y += y;
+      
+      mark_visited(squares, tail->x, tail->y);
+    }
   }
 
   fclose(fptr);
 
-  printf("Squares visited: %d\n", count_visited(squares));
-  
-  // 6411 - too low
-  // 6415 - X
-  // 6416 - X
-  // 7203 - too high
+  printf("Squares visited: %d\n", count_visited(squares)); // 6522
   
   return 0;
 }
