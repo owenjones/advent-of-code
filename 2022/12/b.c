701b752b0495090244d15cdb3e5f90946299ab0b
Owen Jones <owen@owenjones.net>
2022-12-12 21:39:58 +0000

    forgot to actually add the part 2 code...

---
+++ 2022/12/b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pathfind.h"
+
+int main(void) {
+  map_t* map = load_map("input.txt");
+  int steps = find_hiking_trail(map);
+  free_map(map);
+  printf("Number of steps in hiking trail = %d\n", steps); // 439
+  return 0;
+}
