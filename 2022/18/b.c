735d880f3144071f9316573d86e73e56e774c2e8
Owen Jones <owen@owenjones.net>
2022-12-18 22:10:06 +0000

    day 18 part 2: this got messy...

--- 2022/18/b.c
+++ 2022/18/b.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include <inttypes.h>
 
+typedef struct point {
+  int x, y, z;
+} point_t;
+
+typedef struct list {
+  int size;
+  point_t** points;
+} list_t;
+
+void append(list_t* list, point_t* point) {
+  list->points[list->size++] = point;
+}
+
+point_t* pop_head(list_t* list) {
+  point_t* head = list->points[0];
+  for(size_t i = 0; i < list->size; i++) {
+    list->points[i] = list->points[(i + 1)];
+  }
+  list->size--;
+  return head;
+}
+
 int main(void) {
   FILE* fptr;
   if((fptr = fopen("input.txt", "r")) == NULL) {
@@ -10,24 +32,23 @@ int main(void) {
     exit(1);
   }
 
-  uint8_t filled[100][100][100];
+  int filled[100][100][100];
+  int visited[100][100][100];
   size_t x, y, z;
   for(x = 0; x < 100; x++) {
     for(y = 0; y < 100; y++) {
       for(z = 0; z < 100; z++) {
         filled[x][y][z] = 0;
+        visited[x][y][z] = 0;
       }
     }
   }
-
+  
   int minx = 100, maxx = 0, miny = 100, maxy = 0, minz = 100, maxz = 0;
-  int i, j, k, c = 0, cubes[10000][3];
+  int i, j, k;
   while(fscanf(fptr, "%d,%d,%d\n", &i, &j, &k) == 3) {
-    cubes[c][0] = i;
-    cubes[c][1] = j;
-    cubes[c][2] = k;
-    c++;
-    
+    i += 5; j += 5; k += 5; // to move points away from 0
+
     filled[i][j][k] = 1;
     
     minx = (i < minx) ? i : minx;
@@ -39,151 +60,74 @@ int main(void) {
   }
   fclose(fptr);
   
-  for(z = 0; z < 100; z++) {
-    for(y = 0; y < 100; y++) {
-      x = 0;
-      while(x <= 99) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((x + 1) < 100 && filled[(x + 1)][y][z] == 0) {
-          x++;
-          continue;
-        }
-        break;
-      }
-      
-      x = 99;
-      while(x >= 0) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((x - 1) >= 0 && filled[(x - 1)][y][z] == 0) {
-          x--;
-          continue;
-        }
-        break;
-      }
-    }
-    
-    for(x = 0; x < 100; x++) {
-      y = 0;
-      while(y <= 99) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((y + 1) < 100 && filled[x][(y + 1)][z] == 0) {
-          y++;
-          continue;
-        }
-        break;
-      }
-      
-      y = 99;
-      while(y >= 0) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((y - 1) >= 0 && filled[x][(y - 1)][z] == 0) {
-          y--;
-          continue;
-        }
-        break;
-      }
-    }
-  }
+  minx -= 2;
+  maxx += 2;
+  miny -= 2;
+  maxy += 2;
+  minz -= 2;
+  maxz += 2;
   
-  for(z = 99; z >= 0; z--) { 
-    if(z > 99) break;   
-    for(y = 0; y < 100; y++) {
-      x = 0;
-      while(x <= 99) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((x + 1) < 100 && filled[(x + 1)][y][z] == 0) {
-          x++;
-          continue;
-        }
-        break;
-      }
-      
-      x = 99;
-      while(x >= 0) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((x - 1) >= 0 && filled[(x - 1)][y][z] == 0) {
-          x--;
-          continue;
-        }
-        break;
-      }
-    }
-    
-    for(x = 0; x < 100; x++) {
-      y = 0;
-      while(y <= 99) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((y + 1) < 100 && filled[x][(y + 1)][z] == 0) {
-          y++;
-          continue;
-        }
-        break;
-      }
-      
-      y = 99;
-      while(y >= 0) {
-        printf("Filling (%zu, %zu, %zu)\n", x, y, z);
-        filled[x][y][z] = 1;
-        if((y - 1) >= 0 && filled[x][(y - 1)][z] == 0) {
-          y--;
-          continue;
-        }
-        break;
-      }
-    }
-  }
+  list_t* queue = calloc(1, sizeof(list_t));
+  queue->size = 0;
+  queue->points = calloc((maxx * maxy * maxz), sizeof(point_t*));
   
-  // smashing part 2 back into part 1...
-  for(z = 0; z < 100; z++) {
-    for(y = 0; y < 100; y++) {
-      for(x = 0; x < 100; x++) {
-        if(filled[x][y][z] == 0) {
-          printf("Pocket at (%zu, %zu, %zu)\n", x, y, z);
-          // filled[x][y][z] = 1;
+  point_t* point = calloc(1, sizeof(point_t));
+  point->x = minx;
+  point->y = miny;
+  point->z = minz;
+  append(queue, point);
+
+  int faces = 0;
+  point_t *p = NULL, *n = NULL;
+  while(queue->size > 0) {
+    p = pop_head(queue);
+
+    for(x = 0; x < 6; x++) {
+      n = calloc(1, sizeof(point_t));
+      n->x = p->x;
+      n->y = p->y;
+      n->z = p->z;
+      switch(x) {
+        case 0:
+          n->x++;
+          break;
           
-          cubes[c][0] = x;
-          cubes[c][1] = y;
-          cubes[c][2] = z;
-          c++;
-        }
+        case 1:
+          n->x--;
+          break;
+          
+        case 2:
+          n->y++;
+          break;
+          
+        case 3:
+          n->y--;
+          break;
+          
+        case 4:
+          n->z++;
+          break;
+          
+        case 5:
+          n->z--;
+          break;
+      }
+      
+      if(filled[n->x][n->y][n->z]) {
+        faces++;
+      } else if(!visited[n->x][n->y][n->z] 
+        && n->x >= minx - 1
+        && n->x <= maxx + 1
+        && n->y >= miny - 1
+        && n->y <= maxy + 1
+        && n->z >= minz - 1
+        && n->z <= maxz + 1) {
+        visited[n->x][n->y][n->z] = 1;
+        append(queue, n);
       }
     }
   }
-  
-  int adjacent = 0, l, m, n;
-  for(x = 0; x < c; x++) {
-    for(y = 0; y < c; y++) {
-      if(x == y) continue;
-
-      l = (abs(cubes[x][0] - cubes[y][0]) == 1) && (cubes[x][1] == cubes[y][1]) && (cubes[x][2] == cubes[y][2]);
-
-      m = (abs(cubes[x][1] - cubes[y][1]) == 1) && (cubes[x][0] == cubes[y][0]) && (cubes[x][2] == cubes[y][2]);
-
-      n = (abs(cubes[x][2] - cubes[y][2]) == 1) && (cubes[x][1] == cubes[y][1]) && (cubes[x][0] == cubes[y][0]);
-
-      if(l ^ m ^ n) {
-        adjacent++;
-      }
-    }
-  }
-
-  int total = (6 * c) - adjacent;
-  printf("Total surface area: %d\n", total);
-
-  // 1570 - too low
-  // 1876 - too low
-  // 1984 - too low
-  // 2794 - X
-  // 2325 - X
-  // 2865 - X
-  // 3220 - too high
-
+    
+  printf("Faces: %d\n", faces); // 2018
   return 0;
 }
