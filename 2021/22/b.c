8fe908bb600f50bdaecbc732a2d54e3ff8da8b29
Owen Jones <owen@owenjones.net>
2023-01-10 16:24:44 +0000

    more cube

--- 2021/22/b.c
+++ 2021/22/b.c
@@ -11,6 +11,46 @@ typedef struct cube
   int32_t x[2], y[2], z[2];
 } cube_t;
 
+typedef struct list
+{
+  uint32_t elements;
+  cube_t **element;
+} list_t;
+
+cube_t *make_cube(int32_t min_x, int32_t max_x, int32_t min_y, int32_t max_y, int32_t min_z, int32_t max_z, uint8_t value)
+{
+  cube_t *cube = calloc(1, sizeof(cube_t));
+  cube->x[0] = min_x;
+  cube->x[1] = max_x;
+  cube->y[0] = min_y;
+  cube->y[1] = max_y;
+  cube->z[0] = min_z;
+  cube->z[1] = max_z;
+  cube->value = value;
+  return cube;
+}
+
+cube_t *intersection(cube_t *a, cube_t *b)
+{
+  return NULL;
+}
+
+uint64_t volume(cube_t *cube)
+{
+  return (
+      (uint64_t)(cube->x[1] - cube->x[0]) *
+      (uint64_t)(cube->y[1] - cube->y[0]) *
+      (uint64_t)(cube->z[1] - cube->z[0]) *
+      (uint64_t)cube->value);
+}
+
+list_t *make_list(uint32_t elements)
+{
+  list_t *list = calloc(1, sizeof(list_t));
+  list->elements = 0;
+  list->element = calloc(elements, sizeof(cube_t *));
+}
+
 int main(void)
 {
   FILE *fptr;
@@ -24,7 +64,8 @@ int main(void)
   int32_t min_x, max_x, min_y, max_y, min_z, max_z;
   while (fscanf(fptr, "%s x=%i..%i,y=%i..%i,z=%i..%i\n", &action, &min_x, &max_x, &min_y, &max_y, &min_z, &max_z) == 7)
   {
-    uint8_t value = (strcmp(action, "on") == 0);
+    cube_t *cube = make_cube(min_x, max_x, min_y, max_y, min_z, max_z, (strcmp(action, "on") == 0));
+    printf("test volume=%llu\n", volume(cube));
   }
   fclose(fptr);
 
