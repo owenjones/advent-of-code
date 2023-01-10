9bd24189a11725fe0bd3d8d410d6d4dcaa4dcafc
Owen Jones <owen@owenjones.net>
2023-01-10 13:06:31 +0000

    switch position counting to 0-indexed

--- 2021/21/a.c
+++ 2021/21/a.c
@@ -3,36 +3,39 @@
 #include <string.h>
 #include <inttypes.h>
 
-uint32_t roll_dice(uint32_t rolls) {
+uint32_t roll_dice(uint32_t rolls)
+{
   return (rolls % 100) + 1;
 }
 
-int main(void) {
-  FILE* fptr;
-  if((fptr = fopen("input.txt", "r")) == NULL) {
+int main(void)
+{
+  FILE *fptr;
+  if ((fptr = fopen("input.txt", "r")) == NULL)
+  {
     printf("Error opening file\n");
     exit(1);
   }
 
   uint8_t i, s, position[2];
-  while(fscanf(fptr, "Player %hhu starting position: %hhu\n", &i, &s) == 2)
-    position[(i - 1)] = s;
+  while (fscanf(fptr, "Player %hhu starting position: %hhu\n", &i, &s) == 2)
+    position[(i - 1)] = (s - 1);
   fclose(fptr);
 
   uint32_t rolls = 0, d = 1, move, pos;
   uint32_t scores[2] = {0, 0};
 
   uint8_t p = 0;
-  while(1) {
-    move  = roll_dice(rolls++);
+  while (1)
+  {
+    move = roll_dice(rolls++);
     move += roll_dice(rolls++);
     move += roll_dice(rolls++);
 
-    pos = ((position[p] + move) % 10);
-    position[p] = (pos == 0) ? 10 : pos;
-    scores[p] += position[p];
+    position[p] = ((position[p] + move) % 10);
+    scores[p] += position[p] + 1;
 
-    if(scores[p] >= 1000)
+    if (scores[p] >= 1000)
       break;
 
     p = (p + 1) % 2;
