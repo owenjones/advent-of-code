2004ce3e12bc42df9ba20a22a00f5b86896868ce
Owen Jones <owen@owenjones.net>
2023-04-18 17:11:42 +0100

    remove unused line from part 1

--- 2020/02/b.rb
+++ 2020/02/b.rb
@@ -3,8 +3,6 @@ valid = Array.new
 File.open("input.txt") do |input|
   input.each_line do |line|
     e = line.match(/(?<min>\d+)-(?<max>\d+) (?<char>\w{1}): (?<password>\w+)/)
-    count = e[:password].count(e[:char])
-
     first = e[:password][Integer(e[:min]) - 1] == e[:char]
     second = e[:password][Integer(e[:max]) - 1] == e[:char]
 
