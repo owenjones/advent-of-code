430f958b905da48153c9bba111227201e0a92e29
Owen Jones <owen@owenjones.net>
2023-04-19 14:46:41 +0100

    day 7

---
+++ 2020/07/a.rb
@@ -0,0 +1,49 @@
+class RuleList
+    def initialize
+        @bags = Hash.new
+    end
+
+    def addRule(rule)
+        ruleSplit = rule.sub(/\./, "").match(/(?<parent>[a-z ]+) bags contain (?<children>.*)/)
+        children = ruleSplit[:children].split(", ")
+
+        children.each do |child|
+            childSplit = child.match(/(\d+) (?<type>[a-z ]+) bag([s]?)/)
+            if !childSplit.nil?
+                if @bags[childSplit[:type]].nil?
+                    @bags[childSplit[:type]] = Array.new
+                end
+
+                @bags[childSplit[:type]].push(ruleSplit[:parent])
+            end
+        end
+    end
+
+    def getShinyGoldContainerCount
+        containers = Array.new
+
+        def getContainers(parent, containers)
+            if parent.nil?
+                return
+            end
+
+            parent.each do |child|
+                if containers.index(child).nil?
+                    containers.push(child)
+                    getContainers(@bags[child], containers)
+                end
+            end
+        end
+
+        getContainers(@bags["shiny gold"], containers)
+        return containers.size
+    end
+end
+
+rules = RuleList.new
+
+File.open("input.txt") do |input|
+    input.read.split("\n").map { |r| rules.addRule(r) }
+end
+
+puts "Number of possible bags: #{rules.getShinyGoldContainerCount}"
---
+++ 2020/07/b.rb
@@ -0,0 +1,46 @@
+class RuleList
+    def initialize
+        @bags = Hash.new
+    end
+
+    def addRule(rule)
+        ruleSplit = rule.sub(/\./, "").match(/(?<parent>[a-z ]+) bags contain (?<children>.*)/)
+        children = ruleSplit[:children].split(", ")
+
+        if @bags[ruleSplit[:parent]].nil?
+            @bags[ruleSplit[:parent]] = Hash.new
+        end
+
+        children.each do |child|
+            childSplit = child.match(/(?<count>\d+) (?<type>[a-z ]+) bag([s]?)/)
+            if !childSplit.nil?
+                @bags[ruleSplit[:parent]][childSplit[:type]] = Integer(childSplit[:count])
+            end
+        end
+    end
+
+    def getShinyGoldBagCount
+        def getChildCount(parent)
+            if parent.size == 0
+                return 0
+            end
+
+            totalBags = 0
+            parent.each do | child, count |
+                numberOfBags = count + count * getChildCount(@bags[child])
+                totalBags += numberOfBags
+            end
+            return totalBags
+        end
+
+        return getChildCount(@bags["shiny gold"])
+    end
+end
+
+rules = RuleList.new
+
+File.open("input.txt") do |input|
+    input.read.split("\n").map { |r| rules.addRule(r) }
+end
+
+puts "Number of bags inside shiny gold bag: #{rules.getShinyGoldBagCount}"
