54caf7e866b2824e7433992b27674acd443148a2
Owen Jones <owen@owenjones.net>
2023-04-20 15:23:02 +0100

    part 1

--- 2020/09/a.rb
+++ 2020/09/a.rb
@@ -1,4 +1,20 @@
 numbers = Array.new
-File.open("test_input.txt") do |input|
+File.open("input.txt") do |input|
   numbers = input.read.split("\n").map { |x| Integer(x) }
+end
+
+PREAMBLE = 25
+for i in PREAMBLE..(numbers.size-1)
+  preamble = numbers.slice((i-PREAMBLE), PREAMBLE)
+  fails = 0
+  preamble.each do |n|
+    remainder = (numbers[i] - n).abs
+    next if n == remainder
+    break if preamble.include?(remainder)
+    fails += 1
+    if fails == PREAMBLE
+      puts "Not valid: #{numbers[i]}"
+      exit
+    end
+  end
 end
