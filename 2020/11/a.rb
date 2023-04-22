47f80d038b729ab9dbbe4eca63f8a0868e39afa9
Owen Jones <owen@owenjones.net>
2023-04-23 00:45:06 +0100

    day 11 complete, although a pretty messy solution that needs some tidyign

---
+++ 2020/11/a.rb
@@ -0,0 +1,72 @@
+Checks = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+def countOccupied(floor)
+    return floor.map { |x| x.count("#") }.sum
+end
+
+def countAdjacent(input, i, j, type)
+    adjacent = 0
+    Checks.each do |check|
+        next if i + check[0] < 0
+        next if i + check[0] >= input.size
+        next if j + check[1] < 0
+        next if j + check[1] >= input[i].size
+
+        if input[i + check[0]][j + check[1]] == type
+            adjacent += 1
+        end
+    end
+    return adjacent
+end
+
+def printFloor(floor)
+    columns = floor.map { |x| x.join("") }
+    rows = columns.join("\n")
+    puts rows
+    puts
+end
+
+def modelArrivals(input)
+    output = Array.new
+    while true
+        for i in 0..(input.size-1)
+            row = Array.new
+            for j in 0..input[i].size
+                case input[i][j]
+                when "."
+                    row.push(".")
+                when "L"
+                    count = countAdjacent(input, i, j, "#")
+                    if count == 0
+                        row.push("#")
+                    else
+                        row.push("L")
+                    end
+                when "#"
+                    count = countAdjacent(input, i, j, "#")
+                    if count >= 4
+                        row.push("L")
+                    else
+                        row.push("#")
+                    end
+                end
+            end
+            output.push(row)
+        end
+
+        break if output == input
+        input = output.dup
+        output = Array.new
+    end
+    return output
+end
+
+floor = Array.new
+File.open("input.txt") do |input|
+    floor = input.read.split
+end
+floor = floor.map { |x| x.split("") }
+final = modelArrivals(floor)
+occupied = countOccupied(final)
+
+puts "Occupied seats: #{occupied}"
---
+++ 2020/11/b.rb
@@ -0,0 +1,82 @@
+Checks = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+def countOccupied(floor)
+    return floor.map { |x| x.count("#") }.sum
+end
+
+def countAdjacent(input, i, j, type)
+    adjacent = 0
+    Checks.each do |check|
+        l = i + check[0]
+        m = j + check[1]
+        next if l < 0 || l > (input.size-1)
+        next if m < 0 || m >= input[i].size
+
+        while input[l][m] == "."
+            l += check[0]
+            m += check[1]
+            break if l < 0 || l > (input.size-1)
+            break if m < 0 || m >= input[i].size
+        end
+
+        next if l < 0 || l > (input.size-1)
+        next if m < 0 || m >= input[i].size
+
+        if input[l][m] == type
+            adjacent += 1
+        end
+    end
+    return adjacent
+end
+
+def printFloor(floor)
+    columns = floor.map { |x| x.join("") }
+    rows = columns.join("\n")
+    puts rows
+    puts
+end
+
+def modelArrivals(input)
+    output = Array.new
+    while true
+        for i in 0..(input.size-1)
+            row = Array.new
+            for j in 0..input[i].size
+                case input[i][j]
+                when "."
+                    row.push(".")
+                when "L"
+                    count = countAdjacent(input, i, j, "#")
+                    if count == 0
+                        row.push("#")
+                    else
+                        row.push("L")
+                    end
+                when "#"
+                    count = countAdjacent(input, i, j, "#")
+                    if count >= 5
+                        row.push("L")
+                    else
+                        row.push("#")
+                    end
+                end
+            end
+            output.push(row)
+        end
+
+        break if output == input
+        input = output.dup
+        output = Array.new
+    end
+    return output
+end
+
+floor = Array.new
+File.open("input.txt") do |input|
+    floor = input.read.split
+end
+floor = floor.map { |x| x.split("") }
+final = modelArrivals(floor)
+occupied = countOccupied(final)
+
+puts "Occupied seats: #{occupied}"
