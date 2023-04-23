330862819c7c45c78c8f750dd968775f3737412d
Owen Jones <owen@owenjones.net>
2023-04-23 23:20:45 +0100

    didn't need to pad list

--- 2020/10/b.rb
+++ 2020/10/b.rb
@@ -3,16 +3,11 @@ File.open("input.txt") do |input|
   numbers = input.read.split.map { |x| Integer(x) }
 end
 
-rating = numbers.max + 3
 numbers = numbers.sort
-numbers.insert(0, 0)
-numbers.push(rating)
-
 counts = Hash.new(0)
 counts[0] = 1
 
 numbers.each do |n|
-  next if n == 0
   counts[n] = counts[n - 1] + counts[n - 2] + counts[n - 3]
 end
 
