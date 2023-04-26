243e59c61e3e90abda83764f3680da8f96cdbccc
Owen Jones <owen@owenjones.net>
2023-04-26 16:09:15 +0100

    day 15 👍

---
+++ 2020/15/b.rb
@@ -0,0 +1,24 @@
+def saidOnTurn(numbers, stop)
+  lastSaid = Hash.new(nil)
+  numbers.each_with_index { |n,i| lastSaid[n] = [i+1] }
+  turn = numbers.size + 1
+  said = numbers.last
+  while turn <= stop
+    if lastSaid[said].size == 1
+      said = 0
+    else
+      n = lastSaid[said][-2, 2]
+      said = n[1] - n[0]
+    end
+    if lastSaid[said] == nil
+      lastSaid[said] = Array.new
+    end
+    lastSaid[said].push(turn)
+    turn += 1
+  end
+  return said
+end
+
+numbers = File.open("input.txt").read.split(",").map { |x| Integer(x) }
+said = saidOnTurn(numbers, 30000000)
+puts "The number said on turn 30000000: #{said}"
