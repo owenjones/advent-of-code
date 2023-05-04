f9638c2361091141020a90c46ef5456d11dddc21
Owen Jones <owen@owenjones.net>
2023-05-04 15:55:02 +0100

    still not finished

--- 2020/16/b.rb
+++ 2020/16/b.rb
@@ -10,41 +10,31 @@ end
 def findValid(nearby, fields)
   numbers = fields.map { |k, v| v }.flatten.uniq
   tickets = nearby.split("\n").drop(1).map do |ticket|
-    ticket = ticket.split(",")
-    valid = ticket.map { |n| numbers.include?(Integer(n)) }.select { |x| x == true }.size
+    ticket = ticket.split(",").map { |n| Integer(n) }
+    valid = ticket.map { |n| numbers.include?(n) }.select { |x| x == true }.size
     (valid == ticket.size) ? ticket : nil
   end
   return tickets.compact
 end
 
-def testPermutation(permutation, tickets, fields)
-  tickets.each do |ticket|
-    ticket.each.with_index do |n,i| 
-      return nil if !fields[permutation[i]].include?(Integer(n))
-    end
-  end
-  return permutation
-end
-
 def matchFields(valid, fields)
-  permutations = fields.map { |k, v| k }.permutation
-  permutations.each do |permutation|
-    t = testPermutation(permutation, valid, fields)
-    next if t == nil
-    return permutation
-  end
+  # Can we turn tickets into a matrix, then for each column find a valid field (I assume there
+  # can only be one, or there could be multiple valid permutations of the fields?)
 end
 
 def extractDepartureFields(ticket, fields)
   numbers = ticket.split("\n")[1].split(",")
   matched = fields.zip(numbers).select { |x| x[0].match?(/^departure (.*)$/) }
-  summed = matched.map { |x| Integer(x[1]) }
-  return summed
+  numbers = matched.map { |x| Integer(x[1]) }
+  return numbers
 end
 
 blocks = File.open("test_input_2.txt").read.split("\n\n")
 fields = parseFields(blocks[0])
 valid = findValid(blocks[2], fields)
+
+puts valid.to_s
+
 matched = matchFields(valid, fields)
 departureValues = extractDepartureFields(blocks[1], matched)
 answer = departureValues.inject(:*)
