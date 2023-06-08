ec03dc3c46a8d55498d4482c5c1b7708cfd1f16c
Owen Jones <owen@owenjones.net>
2023-06-08 13:20:17 +0100

    classic 'real input is too big to brute force it'

--- 2020/19/a.rb
+++ 2020/19/a.rb
@@ -1,14 +1,20 @@
 def expandRules(rules)
-  expanded = Hash.new(false)
+  expanded = Hash.new
   rules.each do |rule|
     m = rule.match(/(\d+): (.*)/)
-    expanded[Integer(m[1])] = m[2]
+    expanded[Integer(m[1])] = m[2].sub("\"a\"", "a").sub("\"b\"", "b")
   end
   return expanded
 end
 
 def generateRegex(rules)
-  
+  while (n = rules[0].match(/(\d)+/)) != nil
+    r = ["(", rules[Integer(n[1])], ")"].join
+    rules[0].sub!(n[0], r)
+  end
+
+  rules[0].gsub!(/\s+/, "")
+  return /^#{rules[0]}$/
 end
 
 (rules, messages) = File.open("test_input.txt").read.split("\n\n").map { |x| x.split("\n") }
