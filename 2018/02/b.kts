f19b0147822a384b558fc365b6f2e76c059004bc
Owen Jones <owen@owenjones.net>
2023-09-14 12:29:08 +0100

    tidy

--- 2018/02/b.kts
+++ 2018/02/b.kts
@@ -4,8 +4,7 @@ val ids = File("input.txt").bufferedReader().readLines().sorted().map({ id -> id
 
 val p: (Boolean) -> Boolean = { it == false }
 for (i in 0..(ids.size - 2)) {
-  var pairs = (ids[i] zip ids[i+1])
-  
+  val pairs = (ids[i] zip ids[i+1])
   val matching = pairs.map({ (a, b) -> a == b })
   if (matching.count(p) == 1) {
     val letters = matching.indices.filter({ matching[it] == true }).map({ it -> pairs[it].first }).joinToString("")
