01901fbd1e253ba76aea4d50044627075a841138
Owen Jones <owen@owenjones.net>
2023-12-03 02:39:19 +0000

    tidying

--- 2023/02/a.swift
+++ 2023/02/a.swift
@@ -1,11 +1,9 @@
 import Foundation
 let input = try String(contentsOfFile: "input.txt").split(separator: "\n")
-let game = #/Game (\d+): (.*)/#
-let dice = #/(\d+) (red|green|blue)/#
 
 var total = 0
 for line in input {
-  if let match = line.wholeMatch(of: game) {
+  if let match = line.wholeMatch(of: #/Game (\d+): (.*)/#) {
     var possible = true
     let pulls = match.2.split(separator: ";").map{$0.trimmingCharacters(in: .whitespaces)}
     
@@ -13,7 +11,7 @@ for line in input {
       var count: [String: Int] = [:]
       let numbers = hand.split(separator: ",").map{$0.trimmingCharacters(in: .whitespaces)}
       for number in numbers {
-        if let pulled = number.wholeMatch(of: dice) {
+        if let pulled = number.wholeMatch(of: #/(\d+) (red|green|blue)/#) {
           count[String(pulled.2)] = (count[String(pulled.2)] ?? 0) + (Int(pulled.1) ?? 0)
         }
       }
@@ -24,8 +22,9 @@ for line in input {
     }
 
     if(possible) {
-      total = total + Int(match.1)!
+      total = total + (Int(match.1) ?? 0)
     }
   }
 }
+
 print("Sum of IDs from possible games: \(total)") // 2416
--- 2023/02/b.swift
+++ 2023/02/b.swift
@@ -1,23 +1,19 @@
 import Foundation
 let input = try String(contentsOfFile: "input.txt").split(separator: "\n")
-let game = #/Game (\d+): (.*)/#
-let dice = #/(\d+) (red|green|blue)/#
 
 var total = 0
 for line in input {
-  if let match = line.wholeMatch(of: game) {
+  if let match = line.wholeMatch(of: #/Game (\d+): (.*)/#) {
     var count: [String: Int] = [:]
-    let pulled = String(match.2)
-
-    let numbers = pulled.replacingOccurrences(of: ";", with: ",").split(separator: ",").map{$0.trimmingCharacters(in: .whitespaces)}
+    let dice = String(match.2)
+    let numbers = dice.replacingOccurrences(of: ";", with: ",").split(separator: ",").map{$0.trimmingCharacters(in: .whitespaces)}
     for number in numbers {
-      if let pulled = number.wholeMatch(of: dice) {
+      if let pulled = number.wholeMatch(of: #/(\d+) (red|green|blue)/#) {
         count[String(pulled.2)] = max((count[String(pulled.2)] ?? 0), (Int(pulled.1) ?? 0))
       }
     }
-
-    let power = (count["red"] ?? 0) * (count["green"] ?? 0) * (count["blue"] ?? 0)
-    total = total + power
+    total = total + (count["red"] ?? 0) * (count["green"] ?? 0) * (count["blue"] ?? 0)
   }
 }
+
 print("Sum of cube powers: \(total)") // 63307
