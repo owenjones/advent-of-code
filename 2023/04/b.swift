e03c8e60f3d02fbc54f316fe58421fa3206dd83a
Owen Jones <owen@owenjones.net>
2023-12-04 15:56:08 +0000

    more descriptive name

--- 2023/04/b.swift
+++ 2023/04/b.swift
@@ -1,7 +1,7 @@
 import Foundation
 let input = try String(contentsOfFile: "input.txt").split(separator: "\n")
 
-var multiplier: [Int: Int] = [:]
+var cardInstances: [Int: Int] = [:]
 for line in input {
   if let match = line.wholeMatch(of: #/Card ([\w ]+): ([\w ]*) \| ([\w ]*)/#) {
     let id = Int(match.1.trimmingCharacters(in: .whitespaces)) ?? 0
@@ -9,15 +9,15 @@ for line in input {
     let drawn = Set(match.3.split(separator: " ").map{Int($0.trimmingCharacters(in: .whitespaces)) ?? 0})
     let matching = drawn.intersection(winning).count
     
-    multiplier[id] = multiplier[id] ?? 1
+   cardInstances[id] = cardInstances[id] ?? 1
     if matching > 0 {
       for i in (id+1)...(id+matching) {
-        multiplier[i] = multiplier[i] ?? 1
-        multiplier[i]! += multiplier[id] ?? 1
+       cardInstances[i] = cardInstances[i] ?? 1
+       cardInstances[i]! += cardInstances[id] ?? 1
       }
     }
   }
 }
 
-let total = (1...input.count).map{ multiplier[$0]! }.reduce(0, +)
+let total = (1...input.count).map{ cardInstances[$0]! }.reduce(0, +)
 print("Scratchcards: \(total)") // 23806951
