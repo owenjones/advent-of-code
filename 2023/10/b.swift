be69cb0c52040e2e69cec284e371ee9c1ed20195
Owen Jones <owen@owenjones.net>
2023-12-11 01:39:07 +0000

    part 2... spent 30 mins debugging to realise I needed to include the start token...

---
+++ 2023/10/b.swift
@@ -0,0 +1,90 @@
+import Foundation
+let map = try String(contentsOfFile: "input.txt").split(separator: "\n").map { $0.split(separator: "").map{ String($0) } }
+
+/*
+coordinates in (x, y)
+
+(0,0) -> extending y
+|
+v extending x
+*/
+
+let directions = [
+  // ((input), (output)) - assuming clockwise flow
+  "|": ((-1, 0), (1, 0)),
+  "-": ((0, -1), (0, 1)),
+  "L": ((-1, 0), (0, 1)),
+  "J": ((-1, 0), (0, -1)),
+  "7": ((0, -1), (1, 0)),
+  "F": ((0, 1), (1, 0)),
+  ]
+
+// TODO: make allllll of this much nicer...
+
+// Step 1: find the start
+var start: (Int, Int) = (0, 0)
+for x in map.enumerated() {
+  for y in x.element.enumerated() {
+    if y.element == "S" {
+      start = (x.offset, y.offset)
+    }
+  }
+}
+
+// Step 2: find the two valid pipes next to the start
+var nodes: [(Int, Int)] = []
+let cardinals = [
+  (-1, 0, ["|", "7", "F"]),
+  (0, 1, ["-", "7", "J"]),
+  (1, 0, ["|", "L", "J"]),
+  (0, -1, ["-", "L", "F"])
+  ]
+for c in cardinals {
+  let x = start.0 + c.0
+  let y = start.1 + c.1
+  if x >= 0 && y >= 0 && c.2.contains(map[x][y]) {
+    nodes.append((x, y))
+  }
+}
+
+// Step 3: keep stepping along pipe pieces in both clockwise and counter-clockwise directions until we meet
+var last = (start, start)
+var steps = 1
+var cwPath: [(Int, Int)] = [nodes[0]]
+var ccwPath: [(Int, Int)] = [nodes[1]]
+while nodes[0] != nodes[1] {
+  let next = (directions[map[nodes[0].0][nodes[0].1]]!, directions[map[nodes[1].0][nodes[1].1]]!)
+  let cw = [(nodes[0].0 + next.0.0.0, nodes[0].1 + next.0.0.1), (nodes[0].0 + next.0.1.0, nodes[0].1 + next.0.1.1)].filter({ $0 != last.0 })[0]
+  let ccw = [(nodes[1].0 + next.1.0.0, nodes[1].1 + next.1.0.1), (nodes[1].0 + next.1.1.0, nodes[1].1 + next.1.1.1)].filter({ $0 != last.1 })[0]
+  
+  last = (nodes[0], nodes[1])
+  nodes = [cw, ccw]
+  cwPath.append(cw)
+  ccwPath.append(ccw)
+  steps += 1
+}
+
+// Step 4: get all the pipe tiles within the path
+var path = [start]
+path.append(contentsOf: cwPath)
+path.append(contentsOf: ccwPath)
+
+// Step 5: walk each row and sum the tiles which fall within the loop
+var tiles = 0
+for (x, row) in map.enumerated() {
+  var within = false
+  for (y, char) in row.enumerated() {
+    if path.contains(where: { $0 == (x, y) }) {
+      if ["|", "L", "J", "S"].contains(char) {
+        within = !within
+      }
+    }
+    else {
+      if within {
+        tiles += 1
+      }
+    }
+  }
+}
+
+print("Number of tiles enclosed by the loop: \(tiles)")
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Here are my solutions :)
 
 | Year                                  | Language | Stars |
 | :-:                                   | :-:      | :-:   |
-| [2023](https://adventofcode.com/2023) | Swift    | 19     |
+| [2023](https://adventofcode.com/2023) | Swift    | 20     |
 | [2022](https://adventofcode.com/2022) | C        | 50    |
 | [2021](https://adventofcode.com/2021) | C        | 39    |
 | [2020](https://adventofcode.com/2020) | Ruby     | 44    |
