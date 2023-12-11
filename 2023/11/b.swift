4bfc56133cfa1650cf411e88102dc5e442f99c70
Owen Jones <owen@owenjones.net>
2023-12-11 19:06:52 +0000

    part 2 complete via the use of pseudo-universe-expansion

--- 2023/11/b.swift
+++ 2023/11/b.swift
@@ -1,36 +1,16 @@
 import Foundation
 
-let extend = 1000000
-
 var input = try String(contentsOfFile: "input.txt").split(separator: "\n").map{ String($0) }
 
-// Expand universe
+// Find the empty rows/columns (where expansion would happen)
 let allColumns = Set(0..<input[0].count)
 let filledColumns = Set(input.flatMap{ $0.enumerated().compactMap{ $0.element == "#" ? $0.offset : nil } })
 let emptyColumns = allColumns.symmetricDifference(filledColumns).sorted()
 let emptyRows = input.enumerated().compactMap{ ($0.element.contains("#") == false) ? $0.offset : nil }
 
-for (i, r) in emptyRows.enumerated() {
-  for j in 0...(extend - 2) {
-    input.insert(String(repeating: ".", count: input[0].count), at: r + (i * (extend - 1)) + j)
-  }
-}
-
-var universe: [[String]] = []
-for r in input.map({ $0.split(separator: "") }) {
-  var n = r
-  for (i, c) in emptyColumns.enumerated() {
-    for j in 0...(extend - 2) {
-      n.insert(".", at: c + (i * (extend - 1)) + j)
-    }
-  }
-
-  universe.append(n.map{ String($0) })
-}
-
 // Find coordinates of galaxies
 var galaxies: [(Int, Int)] = []
-for (x, r) in universe.enumerated() {
+for (x, r) in input.enumerated() {
   for (y, c) in r.enumerated() {
     if c == "#" {
       galaxies.append((x, y))
@@ -50,7 +30,15 @@ for (i, a) in galaxies.enumerated() {
   }
 }
 
-// Get all lengths and sum
-let lengths = paired.map{ abs($0.0.0 - $0.1.0) + abs($0.0.1 - $0.1.1) }
-let sum = lengths.reduce(0, +)
+// Get all lengths and sum -> if we cross a missing row/column, expand
+let expand = 1000000
+var sum = 0
+for pair in paired {
+  let manhattan = abs(pair.0.0 - pair.1.0) + abs(pair.0.1 - pair.1.1)
+  let x = (pair.0.0 < pair.1.0) ? (pair.0.0...pair.1.0) : (pair.1.0...pair.0.0)
+  let y = (pair.0.1 < pair.1.1) ? (pair.0.1...pair.1.1) : (pair.1.1...pair.0.1)
+  let rows = emptyRows.filter({ x.contains($0) }).count
+  let columns = emptyColumns.filter({ y.contains($0) }).count
+  sum += manhattan + ((rows + columns) * (expand - 1))
+}
 print("Sum of the shortest paths between each galaxy: \(sum)")
