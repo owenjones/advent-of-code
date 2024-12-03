646ed603bb73f017f32a68765811a241e2b7ee1d
Owen Jones <owen@owenjones.net>
2024-12-03 06:03:29 +0000

    moar tidy

--- 2024/03/solve.py
+++ 2024/03/solve.py
@@ -1,17 +1,17 @@
 import re
 
-input = open("input.txt").read()
+input = open("input.txt").read().replace("\n", "")
 
 
-def pairs(input):
+def multiplications(input):
     instructions = re.findall(r"mul\(([\d]+),([\d]+)\)", input)
     return sum(map(lambda x: (int(x[0]) * int(x[1])), instructions))
 
 
-total = pairs(input)
+total = multiplications(input)
 print(f"Part 1: {total}")
 
-input = "do()" + input.replace("\n", "") + "don't()"
+input = "do()" + input + "don't()"
 correct = re.findall(r"do\(\)(.+?(?!=don't\(\)))don't\(\)", input)
-total = pairs("".join(correct))
+total = multiplications("".join(correct))
 print(f"Part 2: {total}")
