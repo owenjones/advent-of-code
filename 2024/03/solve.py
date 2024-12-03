f6adfac0db79af0f79e7baa7405ad05cc7cbf1a4
Owen Jones <owen@owenjones.net>
2024-12-03 06:01:20 +0000

    could simplify this even more

--- 2024/03/solve.py
+++ 2024/03/solve.py
@@ -5,13 +5,13 @@ input = open("input.txt").read()
 
 def pairs(input):
     instructions = re.findall(r"mul\(([\d]+),([\d]+)\)", input)
-    return list(map(lambda x: (int(x[0]), int(x[1])), instructions))
+    return sum(map(lambda x: (int(x[0]) * int(x[1])), instructions))
 
 
-total = sum(map(lambda x: x[0] * x[1], pairs(input)))
+total = pairs(input)
 print(f"Part 1: {total}")
 
 input = "do()" + input.replace("\n", "") + "don't()"
 correct = re.findall(r"do\(\)(.+?(?!=don't\(\)))don't\(\)", input)
-total = sum(map(lambda x: x[0] * x[1], pairs("".join(correct))))
+total = pairs("".join(correct))
 print(f"Part 2: {total}")
