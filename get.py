77128101998e824a2b2151e5e477b692325b3270
Owen Jones <owen@owenjones.net>
2024-12-10 14:28:30 +0000

    only make dir if file fetched; fix newline at end of inputs; drop dotenv

--- get.py
+++ get.py
@@ -1,26 +1,25 @@
 import urllib.request
 from pathlib import Path
-from dotenv import dotenv_values
+import json
 import argparse
 
-def fetch(year, day):
+def fetch(year, day, cookie):
     pday = f"0{day}" if (int(day) < 10) else day
     url = f"https://adventofcode.com/{year}/day/{day}/input"
     dir = Path(__file__).parent / str(year) / f"{pday}"
     output = dir / "input.txt"
 
-    if not dir.exists():
-        dir.mkdir()
-
     request = urllib.request.Request(url)
-    cookie = dotenv_values(".env")["SESSION"]
     request.add_header("Cookie", f"session={cookie}")
 
     try:
         with urllib.request.urlopen(request) as response:
             input = response.read().decode("utf-8")
             with open(output, "w") as f:
-                f.write(input)
+                if not dir.exists():
+                    dir.mkdir()
+
+                f.write(input[:-1:])
 
     except urllib.error.HTTPError:
         print("HTTP Error")
@@ -32,4 +31,17 @@ if __name__ == "__main__":
     parser.add_argument("day")
     args = parser.parse_args()
 
-    fetch(args.year, args.day)
+    if not (0 <= int(args.day) <= 25):
+        exit("Day not valid for Advent of Code")
+
+    if not (2015 <= int(args.year)):
+        exit("Year not valid for Advent of Code")
+
+    config = Path(__file__).parent / "config.json"
+
+    if not config.exists():
+        exit("Config file not found")
+
+    session = json.loads(open(config).read()).get("cookie", "")
+
+    fetch(args.year, args.day, session)
