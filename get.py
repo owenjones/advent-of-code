d7bb75800275574ef54a12308aafe54acac92044
Owen Jones <owen@owenjones.net>
2024-12-18 14:44:55 +0000

    fills in missing directories and checks year/day

--- get.py
+++ get.py
@@ -5,10 +5,19 @@ import argparse
 
 def fetch(year, day, cookie):
     pday = f"0{day}" if (int(day) < 10) else day
-    url = f"https://adventofcode.com/{year}/day/{day}/input"
-    dir = Path(__file__).parent / str(year) / f"{pday}"
+    dir = Path(__file__).parent / str(year)
+
+    if not dir.exists():
+        dir.mkdir()
+
+    dir = dir / f"{pday}"
+
+    if not dir.exists():
+        dir.mkdir()
+
     output = dir / "input.txt"
 
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
     request = urllib.request.Request(url)
     request.add_header("Cookie", f"session={cookie}")
 
@@ -16,9 +25,6 @@ def fetch(year, day, cookie):
         with urllib.request.urlopen(request) as response:
             input = response.read().decode("utf-8")
             with open(output, "w") as f:
-                if not dir.exists():
-                    dir.mkdir()
-
                 f.write(input[:-1:])
 
     except urllib.error.HTTPError:
@@ -27,21 +33,20 @@ def fetch(year, day, cookie):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("year")
-    parser.add_argument("day")
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
     args = parser.parse_args()
 
-    if not (0 <= int(args.day) <= 25):
-        exit("Day not valid for Advent of Code")
-
-    if not (2015 <= int(args.year)):
-        exit("Year not valid for Advent of Code")
-
     config = Path(__file__).parent / "config.json"
-
     if not config.exists():
         exit("Config file not found")
 
     session = json.loads(open(config).read()).get("cookie", "")
+
+    if not (2015 <= args.year):
+        exit("Year not valid for Advent of Code")
+
+    if not (0 <= args.day <= 25):
+        exit("Day not valid for Advent of Code")
 
     fetch(args.year, args.day, session)
