a44f6e10949c623af4fb9e1d9f50bd761b89e5b7
Owen Jones <owen@owenjones.net>
2025-01-24 01:11:00 +0000

    split snailfish stuff off for part 2

---
+++ 2021/18/snailfish.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+typedef struct node
+{
+  int8_t value;
+  uint8_t depth;
+  struct node *parent;
+  struct node *left;
+  struct node *right;
+  uint8_t ud;
+  uint8_t lr;
+} node_t;
+
+void debug(const char *format, ...);
+void to_string(node_t *node);
+void _to_string(node_t *node);
+
+void init_node(node_t *node);
+void parse_line(node_t *root, char *line);
+
+void reduce(node_t *root);
+
+uint8_t explode(node_t *node);
+uint8_t split(node_t *node);
+
+void increase_depth(node_t *node);
+
+node_t *add_numbers(node_t *left, node_t *right);
+uint32_t calculate_magnitude(node_t *node);
